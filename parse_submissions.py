8700d463b3f18594a605e2d666e9a4a4a8685b8b
Kristófer Gudmundsson <kristofergudmundsson@Kristofers-MacBook-Pro.local>
2018-12-08 15:18:09 +0000

    Functionality completed

--- parse_submissions.py
+++ parse_submissions.py
@@ -1,12 +1,14 @@
 import os
 
 def parse_submissions(directory):
+    # retrieve datafiles with os.walk
     datafiles = []
     for root, dirs, files in os.walk(directory):
         for f in files:
-            if f.endswith(".tcl"):
+            if f.endswith(".tcl"): # all data files end with .tcl
                 datafiles.append(os.path.join(root, f))
-
+    
+    # narrow to accepted files only
     accepted = []
     for d in datafiles:
         with open(d, "r") as f:
@@ -21,12 +23,20 @@ def parse_submissions(directory):
     for a in accepted:
         with open(a, "r") as f:
             for line in f:
+                # split three important line in three parts by the key phrases below
+                # wanted data after phrase
+                # create a tuple of the data and add to a list
+                if ('Date ' in line):
+                    s, d, date = line.partition('Date ')
                 if ('Problem ' in line):
                     s, p, problem = line.partition('Problem ')
                 if ('Team ' in line):
                     s, t, team = line.partition('Team ')
-                    acc = (team.rstrip(), problem.rstrip())
+                    acc = (date.rstrip(), team.rstrip(), problem.rstrip())
                     submissions.append(acc)
-    return submissions
-
-print(parse_submissions('submissions'))
+    # sort list by date, remove date
+    submissions = sorted(submissions)
+    datelessSubs = []
+    for s in submissions:
+        datelessSubs.append((s[1],s[2]))
+    return datelessSubs
